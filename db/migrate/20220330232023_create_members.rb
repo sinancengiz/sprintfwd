9f0f8eba06aec0c3995aa9cd7b9be2a0a26f6efa
Sinan Cengiz <sinancengiz_84@hotmail.com>
2022-03-30 17:41:36 -0700

    Project model is created

--- db/migrate/20220330232023_create_members.rb
+++ db/migrate/20220330232023_create_members.rb
@@ -1,12 +1,12 @@
 class CreateMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :members do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, presence:true
+      t.string :last_name, presence:true
       t.string :city
       t.string :state
       t.string :country
-      t.integer :team_id
+      t.integer :team_id, presence:true
 
       t.timestamps
     end
--- db/migrate/20220331003918_create_projects.rb
+++ db/migrate/20220331003918_create_projects.rb
@@ -1,6 +1,6 @@
-class CreateTeams < ActiveRecord::Migration[5.2]
+class CreateProjects < ActiveRecord::Migration[5.2]
   def change
-    create_table :teams do |t|
+    create_table :projects do |t|
       t.string :name, presence:true
 
       t.timestamps
