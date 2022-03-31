e71a4ce135a4a5a812e8682af72a2a017ef347c9
Sinan Cengiz <sinancengiz_84@hotmail.com>
2022-03-30 18:22:53 -0700

    team controller is created

---
+++ app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::TeamsController < ApplicationController
+    before_action :set_team, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @teams = Team.all
+        render json: @teams
+    end
+
+    def show
+        render json: @team
+    end
+
+    def create
+        @team = Team.new(team_params)
+        if @team.save
+            render json: @team
+        else
+            render json: @team.errors.full_messages, status: 422
+        end
+    rescue ActiveRecord::NotNullViolation => e
+        render json: { error: e.message }, status: 422
+    end
+
+    def update
+        if @team.update(team_params)
+            render json: @team
+        else
+            render json: @team.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        if @team.destroy
+          render json: { success: "Team is deleted" }
+        else
+          render json: { error: "Team could not be deleted" }, status: 422
+        end
+      end
+
+    private
+
+    def set_team
+      @team = Team.find(params[:id])
+    end
+  
+    # Only allow a list of trusted parameters through.
+    def team_params
+      params.permit(:name)
+    end
+end
