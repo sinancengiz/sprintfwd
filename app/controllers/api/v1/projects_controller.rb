3435528f21c562852bdd9c7102b6c21be7bdc766
Sinan Cengiz <sinancengiz_84@hotmail.com>
2022-03-30 20:03:20 -0700

    project controller is created

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,54 @@
+class Api::V1::ProjectsController < ApplicationController
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @projects = Project.all
+        render json: @projects
+    end
+
+    def show
+        render json: @project
+    end
+
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+            render json: @project
+        else
+            render json: @project.errors.full_messages, status: 422
+        end
+    rescue ActiveRecord::NotNullViolation => e
+        render json: { error: e.message }, status: 422
+    end
+
+    def update
+        if @project.update(project_params)
+            render json: @project
+        else
+            render json: @project.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        if @project.destroy
+          render json: { success: "Project is deleted" }
+        else
+          render json: { error: "Project could not be deleted" }, status: 422
+        end
+      end
+
+    private
+
+    def set_project
+        begin
+            @project = Project.find(params[:id])
+        rescue
+            render json: { error: "Project not found" }, status: 422
+        end
+    end
+  
+    # Only allow a list of trusted parameters through.
+    def project_params
+      params.permit(:name)
+    end
+end
